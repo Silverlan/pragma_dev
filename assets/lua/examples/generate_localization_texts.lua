151304a6c19ae265574553bb619e87258beaf268
Silverlan <silverlann@gmail.com>
2023-07-24 11:59:33 +0200

    feat: add comment

--- assets/lua/examples/generate_localization_texts.lua
+++ assets/lua/examples/generate_localization_texts.lua
@@ -3,4 +3,5 @@ include("/util/locale/localization_helper.lua")
 local apiKey = "*********CHAT GPT API KEY*********"
 local targetLanguage = "fr"
 
+-- Note that running this function will use up ChatGPT tokens, which come with a fee. Check ChatGPT Pricing for more information.
 util.locale.generate_missing_localizations(apiKey, targetLanguage)
