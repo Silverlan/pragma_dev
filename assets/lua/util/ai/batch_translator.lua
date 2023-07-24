c5101f9fee0c78b2d904b67aab161e6fa4f93825
Silverlan <silverlann@gmail.com>
2023-07-24 11:50:54 +0200

    feat: add localization helper functions

---
+++ assets/lua/util/ai/batch_translator.lua
@@ -0,0 +1,55 @@
+--[[
+    Copyright (C) 2023 Silverlan
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+]]
+
+include("query.lua")
+
+util.ai = util.ai or {}
+local BatchTranslator = util.register_class("util.ai.BatchTranslator")
+function BatchTranslator:__init(apiKey)
+	self.m_apiKey = apiKey
+	self.m_queries = {}
+end
+
+function BatchTranslator:Clear()
+	self.m_queries = {}
+	self.m_cancelled = true
+end
+
+function BatchTranslator:Process()
+	if self.m_running or #self.m_queries == 0 then
+		return
+	end
+	local queryData = self.m_queries[1]
+	table.remove(self.m_queries, 1)
+	self.m_running = true
+	util.ai.translate(self.m_apiKey, queryData.englishText, queryData.targetLanguage, function(res, data)
+		if res then
+			if self.m_cancelled then
+				return
+			end
+			print(
+				"Adding localization for '" .. queryData.id .. "' for language '" .. queryData.targetLanguage .. "'..."
+			)
+			locale.localize(queryData.id, queryData.targetLanguage, queryData.category, data)
+			self.m_running = false
+			self:Process()
+		else
+			console.print_warning("Localization failed...")
+		end
+	end)
+end
+
+function BatchTranslator:Add(cat, id, englishText, targetLanguage)
+	table.insert(self.m_queries, {
+		category = cat,
+		id = id,
+		englishText = englishText,
+		targetLanguage = targetLanguage,
+	})
+	self:Process()
+end
---
+++ assets/lua/util/locale/localization_helper.lua
@@ -0,0 +1,66 @@
+--[[
+    Copyright (C) 2023 Silverlan
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+]]
+
+include("/util/ai/batch_translator.lua")
+
+util.locale = util.locale or {}
+
+function util.locale.find_missing_localizations(targetLanguage)
+	local function get_language_texts(lan)
+		local tTextFiles = file.find("scripts/localization/" .. lan .. "/texts/*.txt")
+		local catTexts = {}
+		for _, f in ipairs(tTextFiles) do
+			local texts = locale.parse(f, lan)
+			local catName = file.remove_file_extension(file.get_file_name(f))
+			catTexts[catName] = texts
+		end
+		return catTexts
+	end
+
+	local en = get_language_texts("en")
+
+	local tgt = get_language_texts(targetLanguage)
+	local result = {}
+	for cat, texts in pairs(en) do
+		local missingIds = {}
+		local catTgt = tgt[cat]
+		if catTgt ~= nil then
+			for id, text in pairs(texts) do
+				if catTgt[id] == nil then
+					table.insert(missingIds, id)
+				end
+			end
+		else
+			for id, text in pairs(texts) do
+				table.insert(missingIds, id)
+			end
+		end
+		if not table.is_empty(missingIds) then
+			result[cat] = missingIds
+		end
+	end
+	return result
+end
+
+function util.locale.generate_missing_localizations(apiKey, targetLanguage)
+	local curLan = locale.get_language()
+	if curLan ~= "en" then
+		error('Current language must be set to "en", but is "' .. curLan .. '"!')
+		return
+	end
+	local batchTranslator = util.ai.BatchTranslator(apiKey)
+	local missingLocs = util.locale.find_missing_localizations(targetLanguage)
+	for cat, missingIds in pairs(missingLocs) do
+		for _, id in ipairs(missingIds) do
+			local englishText = locale.get_raw_text(id)
+			if englishText ~= nil then
+				batchTranslator:Add(cat, id, englishText, targetLanguage)
+			end
+		end
+	end
+end
